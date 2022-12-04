e49e99ad357065ce5c0ce898371ef59483d189b0
babsonpython <112523357+babsonpython@users.noreply.github.com>
2022-12-04 16:25:56 -0500

    finished code

--- twilio_text_message.py
+++ twilio_text_message.py
@@ -3,7 +3,6 @@ from credentials import WILSON_SID, WILSON_AUTH_TOKEN, NUMBERS
 
 wilson_sid = WILSON_SID
 wilson_auth = WILSON_AUTH_TOKEN
-numbers = NUMBERS
 
 
 account_sid = wilson_sid
@@ -11,12 +10,11 @@ auth_token = wilson_auth
 client = Client(account_sid, auth_token)
 
 def twilio_text():
-    message = client.messages \
-        .create(
-            body='AS50 from Badminton Warehouse is now available for purchase!!!',
-            from_='+18647320570',
-            to= numbers
-        )
+    for number in NUMBERS:
+        message = client.messages \
+            .create(
+                body='AS50 from Badminton Warehouse is now available for purchase!!!',
+                from_='+18647320570',
+                to= number
+            )
     return message.sid
-
-print(twilio_text())
--- web_scraping.py
+++ web_scraping.py
@@ -3,10 +3,13 @@ import urllib.request
 from bs4 import BeautifulSoup
 import ssl
 from twilio_text_message import twilio_text
+import schedule
+import time
+import string
 
 
-
-DOWNLOAD_URL = 'https://www.badmintonwarehouse.com/products/yonex-as50-badminton-shuttles'
+AS50_URL = 'https://www.badmintonwarehouse.com/products/yonex-as50-badminton-shuttles'
+RACKET_URL = 'https://www.badmintonwarehouse.com/collections/deals-of-the-day/products/victor-light-fighter-ultra-badminton-racket'
 
 
 def download_page(url):
@@ -15,18 +18,36 @@ def download_page(url):
 
 # print(download_page(DOWNLOAD_URL).read())
 
-def parse_html(html):
+def check_availability(html):
     """
     Analyze the html page, find the information and return the move list of tuples (movie_name, year)
     """
     soup = BeautifulSoup(html, features="html.parser")
-    print(soup.prettify())
-    for atc in 
+    # print(soup.prettify())
+    for atc_string in soup.find_all('span', attrs = {"class":"atc-button--text"}):
+        atc_string = str(atc_string.text).replace(' ','').strip('\n')
+        # print(point)
+    if atc_string == 'Soldout':
+        return 'Unvailable'
+    else:
+        return 'Available'
 
-parse_html(download_page(DOWNLOAD_URL).read())
+# check_availability(download_page(RACKET_URL).read())
+
+def send_text_if_available():
+    if check_availability(download_page(AS50_URL)) == 'Available':
+        twilio_text()
+    else:
+        print('Unavailble for now')
+
+# send_text_if_available()
+
 
 def main():
-    url = DOWNLOAD_URL
+    schedule.every(1).hour.do(send_text_if_available)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     
 
 
