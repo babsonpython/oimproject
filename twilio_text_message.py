ab7b832a516c7fc46ae14542430f36390ba3a062
babsonpython <112523357+babsonpython@users.noreply.github.com>
2022-12-03 23:58:18 -0500

    update

--- twilio_text_message.py
+++ twilio_text_message.py
@@ -1,14 +1,11 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
 from twilio.rest import Client
-from credentials import WILSON_SID, WILSON_AUTH_TOKEN, WILSON_NUMBER
+from credentials import WILSON_SID, WILSON_AUTH_TOKEN, NUMBERS
 
 wilson_sid = WILSON_SID
 wilson_auth = WILSON_AUTH_TOKEN
-wilson_number = WILSON_NUMBER
+numbers = NUMBERS
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
+
 account_sid = wilson_sid
 auth_token = wilson_auth
 client = Client(account_sid, auth_token)
@@ -18,7 +15,7 @@ def twilio_text():
         .create(
             body='AS50 from Badminton Warehouse is now available for purchase!!!',
             from_='+18647320570',
-            to= wilson_number
+            to= numbers
         )
     return message.sid
 
